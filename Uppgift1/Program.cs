8915b7bf2afd07331185ba25287957dc823bf275
Hugge25 <hugo.paulsson@hotmail.com>
2023-11-24 11:13:04 +0100

    aight

--- Uppgift1/Program.cs
+++ Uppgift1/Program.cs
@@ -6,4 +6,4 @@ string ord = Console.ReadLine();
 Console.Write("Antal upprepningar ? ");
 int antal = int.Parse(Console.ReadLine());
     
-Console.WriteLine($"Svar: {String.Concat(Enumerable.Repeat(ord, antal))}");
+Console.WriteLine($"\nSvar: {String.Concat(Enumerable.Repeat(ord, antal))}");
